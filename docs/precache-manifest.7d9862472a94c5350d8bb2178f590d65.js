92c0f6bd64c0c0f844f662b0030f8f43f385e8e1
Carlos Cordoba <carlos.cv9710@gmail.com>
2020-08-17 21:28:50 -0600

    Add Docs

---
+++ docs/precache-manifest.7d9862472a94c5350d8bb2178f590d65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41ddc517efb57a299f6d03cf3f6a9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35842d9440090694c8e",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "dd6d19e1a80c115ae168",
+    "url": "/static/css/main.5db66031.chunk.css"
+  },
+  {
+    "revision": "a35842d9440090694c8e",
+    "url": "/static/js/2.5c8e4ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c8e4ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6d19e1a80c115ae168",
+    "url": "/static/js/main.feef8f8c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
